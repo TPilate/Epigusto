04b295675e2075cc8c2dbf15ecfcd0bd67703c8c
SnowDiles <brahimboulahia59@gmail.com>
2025-04-05 13:51:35 +0200

    [feat] game

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -1,36 +1,71 @@
+import * as Phaser from 'phaser';
 import { EventBus } from '../EventBus';
-import { Scene } from 'phaser';
 
-export class Game extends Scene
-{
-    camera: Phaser.Cameras.Scene2D.Camera;
-    background: Phaser.GameObjects.Image;
-    gameText: Phaser.GameObjects.Text;
+export class Game extends Phaser.Scene {
+    camera!: Phaser.Cameras.Scene2D.Camera;
+    background!: Phaser.GameObjects.Image;
+    gameText!: Phaser.GameObjects.Text;
+    player!: Phaser.Physics.Arcade.Sprite;
+    physics!: Phaser.Physics.Arcade.ArcadePhysics;
 
-    constructor ()
-    {
+
+    constructor() {
         super('Game');
     }
 
-    create ()
-    {
-        this.camera = this.cameras.main;
-        this.camera.setBackgroundColor(0x00ff00);
+    preload() {
+        this.load.spritesheet('player', './assets/PersonaggioFoglioSprite.png', {
+            frameWidth: 80,
+            frameHeight: 150
+        });
+        this.load.image('background', './assets/sfondo.png');
+
+    }
+
+    create() {
+        console.log(this.physics);
 
         this.background = this.add.image(512, 384, 'background');
         this.background.setAlpha(0.5);
+        this.player = this.physics.add.sprite(500, 750, 'player');
 
-        this.gameText = this.add.text(512, 384, 'Pasta !!! ', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5).setDepth(100);
+        this.camera = this.cameras.main;
+
+        this.anims.create({
+            key: 'walk',
+            frames: this.anims.generateFrameNumbers('player', {
+                start: 0,
+                end: 2
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+
 
         EventBus.emit('current-scene-ready', this);
     }
 
-    changeScene ()
-    {
+    changeScene() {
         this.scene.start('GameOver');
     }
+
+
+
+
 }
+
+const config: Phaser.Types.Core.GameConfig = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { x: 0, y: 300 },
+            debug: false
+        }
+    },
+    scene: [Game]
+};
+new Phaser.Game(config);
