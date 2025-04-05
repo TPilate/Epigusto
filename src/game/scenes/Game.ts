244f3ee469f8c2deb767e2a1052758a275ea0120
Gus <augustin.verissimo@epitech.eu>
2025-04-05 17:36:06 +0200

    feat/add life (#30)
    * feat/add life
    
    * refactor/translate
    
    * fix/optimize life increment
    
    * fix/change size of font

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -12,9 +12,11 @@ export class Game extends Phaser.Scene {
     kya: any;
     punteggio: number;
     testoPunteggio: Phaser.GameObjects.Text;
+    vita: number;
+    testoVita: Phaser.GameObjects.Text;
     private velocitaMassima: number;
     private intervalloIncremento: number;
-    private punteggioTarget: number; 
+    private punteggioTarget: number;
     private incrementoPunteggio: number;
     private timerIncrementoPunteggio: Phaser.Time.TimerEvent;
 
@@ -26,6 +28,7 @@ export class Game extends Phaser.Scene {
         this.incrementoPunteggio = 2;
         this.velocitaMassima = 5.0;
         this.intervalloIncremento = 5000;
+        this.vita = 0;
     }
 
     preload() {
@@ -35,7 +38,8 @@ export class Game extends Phaser.Scene {
             frameHeight: 16
         });
         this.load.image('pezzo', 'assets/pezzo.png');
-        this.load.atlas('character', './assets/PersonaggioFoglioSprite.png','./assets/PersonaggioFoglio.json');
+        this.load.image('cuore', 'assets/cuore.png');
+        this.load.atlas('character', './assets/PersonaggioFoglioSprite.png', './assets/PersonaggioFoglio.json');
     }
 
     create() {
@@ -54,7 +58,7 @@ export class Game extends Phaser.Scene {
         );
         this.suolo.setOrigin(0, 0);
         this.suolo.setScale(3);
-        
+
         this.Giocatore = this.physics.add.sprite(80, 200, 'character');
         this.Giocatore.setCollideWorldBounds(true);
         this.physics.world.setBounds(0, 0, this.cameras.main.width, this.cameras.main.height - 155);
@@ -86,15 +90,34 @@ export class Game extends Phaser.Scene {
         });
         this.testoPunteggio.setScrollFactor(0);
 
+        // Vie UI
+        this.add.image(this.cameras.main.width - 175, 5, 'cuore').setOrigin(0, 0).setScale(0.2);
+        this.testoVita = this.add.text(
+            this.cameras.main.width - 75,
+            10,
+            '0', {
+            fontFamily: 'minecraft',
+            fontSize: '52px',
+            color: '#fff'
+        });
+        this.testoVita.setScrollFactor(0);
+
+        this.time.addEvent({
+            delay: 1000,
+            callback: this.aumentaVita,
+            callbackScope: this,
+            loop: true
+        });
+
         this.cursori = this.input.keyboard?.createCursorKeys() || {} as Phaser.Types.Input.Keyboard.CursorKeys;
 
         this.kya = this.input.keyboard?.addKeys({
             z: Phaser.Input.Keyboard.KeyCodes.Z,
             spaceBar: Phaser.Input.Keyboard.KeyCodes.SPACE,
         });
-        
+
         this.Giocatore.play('run');
-        
+
         // Score timer
         this.time.addEvent({
             delay: 1000,
@@ -111,7 +134,7 @@ export class Game extends Phaser.Scene {
             callbackScope: this,
             loop: true
         });
-        
+
         // Speed increase timer
         this.time.addEvent({
             delay: this.intervalloIncremento,
@@ -124,9 +147,14 @@ export class Game extends Phaser.Scene {
     }
 
     update() {
+        if (this.cursori.left?.isDown) {
+            this.velocitaCorrente += 0.01;
+            console.log('Current speed: ' + this.velocitaCorrente);
+        }
+
         this.sfondo.tilePositionX += this.velocitaCorrente;
         this.suolo.tilePositionX += this.velocitaCorrente;
-        
+
 
         const ilGiocatoreSulTerreno = this.Giocatore.body ? (this.Giocatore.body.touching.down || this.Giocatore.body.blocked.down) : false;
         if ((this.kya?.spaceBar?.isDown || this.cursori.space?.isDown) && ilGiocatoreSulTerreno) {
@@ -136,15 +164,15 @@ export class Game extends Phaser.Scene {
             this.time.delayedCall(500, () => {
                 this.Giocatore.play('run', true);
             });
-
+            this.Giocatore.setBounce(0);
         }
-        
+
         if (ilGiocatoreSulTerreno && this.Giocatore.body && this.Giocatore.body.velocity.y === 0) {
             if (this.Giocatore.anims.currentAnim?.key !== 'run') {
                 this.Giocatore.play('run', true);
             }
         }
-        
+
     }
 
     calcolaNuovoPunteggioTarget() {
@@ -155,13 +183,24 @@ export class Game extends Phaser.Scene {
     aggiornaDisplayPunteggio() {
         if (this.punteggio < this.punteggioTarget) {
             this.punteggio += this.incrementoPunteggio;
-            
+
             if (this.punteggio > this.punteggioTarget) {
                 this.punteggio = this.punteggioTarget;
             }
-            
+
             this.testoPunteggio.setText('' + this.punteggio);
         }
+    }
+
+    aumentaVita() {
+        if (this.vita >= 10) {
+            this.testoVita.setText('∞');
+            this.testoVita.setPosition(this.cameras.main.width - 80, 8);
+            return;
+        }
+
+        this.vita += 1;
+        this.testoVita.setText('' + this.vita);
     }
 
     cambiaScena() {
@@ -171,7 +210,7 @@ export class Game extends Phaser.Scene {
     private aumentareVelocita(): void {
         const valoreIncremento = 0.2;
         this.velocitaCorrente += valoreIncremento;
-        
+
         if (this.velocitaCorrente > this.velocitaMassima) {
             this.velocitaCorrente = this.velocitaMassima;
         }
