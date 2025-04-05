b3b720a2abf2c1e638067e71b4edbea85a427b78
Boulahia brahim <98060234+SnowDiles@users.noreply.github.com>
2025-04-05 16:45:00 +0200

    Feat/game   character (#31)
    * [feat] game
    
    * fix/resolve physics
    
    * [fix] correct changes
    
    * [feat] add the jump handling
    
    * [refactor] translate
    
    ---------
    
    Co-authored-by: Augustin Verissimo <augustin.verissimo@epitech.eu>

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -1,43 +1,44 @@
+import * as Phaser from 'phaser';
 import { EventBus } from '../EventBus';
-import { Scene } from 'phaser';
 
-export class Game extends Scene
-{
+export class Game extends Phaser.Scene {
     camera: Phaser.Cameras.Scene2D.Camera;
     sfondo: Phaser.GameObjects.TileSprite;
     testoGioco: Phaser.GameObjects.Text;
-    suolo: Phaser.GameObjects.TileSprite; 
+    suolo: Phaser.GameObjects.TileSprite;
     velocitaCorrente: number;
-
+    cursori: Phaser.Types.Input.Keyboard.CursorKeys;
+    Giocatore: Phaser.Physics.Arcade.Sprite;
+    kya: any;
     private velocitaMassima: number;
     private intervalloIncremento: number;
 
 
-    constructor ()
-    {
+    constructor() {
         super('Game');
         this.velocitaCorrente = 0.5;
         this.velocitaMassima = 5.0;
         this.intervalloIncremento = 5000;
     }
 
-    preload () 
-    {
+    preload() {
         this.load.image('sfondo', 'assets/sfondo.png');
         this.load.spritesheet('suelo', 'assets/suolo.png', {
             frameWidth: 16,
             frameHeight: 16
-          });
+        });
+
+        this.load.atlas('character', './assets/PersonaggioFoglioSprite.png','./assets/PersonaggioFoglio.json');
     }
 
-    create ()
-    {
+    create() {
+
         this.camera = this.cameras.main;
 
         this.sfondo = this.add.tileSprite(0, 0, this.cameras.main.width, this.cameras.main.height, 'sfondo');
         this.sfondo.setOrigin(0, 0);
         this.sfondo.setAlpha(0.5);
-        
+
         this.suolo = this.add.tileSprite(
             0,
             this.cameras.main.height - 40,
@@ -47,7 +48,35 @@ export class Game extends Scene
         );
         this.suolo.setOrigin(0, 0);
         this.suolo.setScale(3);
+        this.Giocatore = this.physics.add.sprite(80, 200, 'character');
+        this.Giocatore.setCollideWorldBounds(true);
+        this.physics.world.setBounds(0, 0, this.cameras.main.width, this.cameras.main.height - 155);
+        this.Giocatore.setBounce(0.3);
+        this.Giocatore.setGravityY(800);
 
+        this.anims.create({
+            key: 'run',
+            frames: this.anims.generateFrameNames('character', { prefix: 'run', end: 4, zeroPad: 2 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'jump',
+            frames: this.anims.generateFrameNames('character', { prefix: 'jump', end: 3, zeroPad: 2 }),
+            frameRate: 20,
+            repeat: 0
+        });
+
+
+        this.cursori = this.input.keyboard?.createCursorKeys() || {} as Phaser.Types.Input.Keyboard.CursorKeys;
+
+        this.kya = this.input.keyboard?.addKeys({
+            z: Phaser.Input.Keyboard.KeyCodes.Z,
+            spaceBar: Phaser.Input.Keyboard.KeyCodes.SPACE,
+        });
+        
+        this.Giocatore.play('run');   
         this.time.addEvent({
             delay: this.intervalloIncremento,
             callback: this.aumentareVelocita,
@@ -61,10 +90,20 @@ export class Game extends Scene
     update() {
         this.sfondo.tilePositionX += this.velocitaCorrente;
         this.suolo.tilePositionX += this.velocitaCorrente;
+        
+      
+        
+        if (this.kya?.spaceBar?.isDown) {
+            this.Giocatore.setVelocityY(-600);
+            this.Giocatore.play('jump', true);
+        }
+        
+        if (!this.cursori.space.isDown) {
+            this.Giocatore.play('run', true);
+        }
     }
 
-    cambiaScena ()
-    {
+    cambiaScena() {
         this.scene.start('GameOver');
     }
 
