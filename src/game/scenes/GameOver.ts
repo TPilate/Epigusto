b627dcd5111a153723b77527239af89c3e874838
Augustin Verissimo <augustin.verissimo@epitech.eu>
2025-04-06 17:01:00 +0200

    fix/add minecraft font for gameover and scoreboard

--- src/game/scenes/GameOver.ts
+++ src/game/scenes/GameOver.ts
@@ -24,7 +24,7 @@ export class GameOver extends Scene {
         this.sfondo.setAlpha(0.5);
 
         this.testoFinale = this.add.text(512, 384, 'Game Over', {
-            fontFamily: 'Arial Black', fontSize: 64, color: '#ffffff',
+            fontFamily: 'minecraft', fontSize: 64, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
         }).setOrigin(0.5).setDepth(100);
